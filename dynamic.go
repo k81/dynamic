f9afaf48edfe96aa7718e8b356ef0f6a09540495
kaizhangkai <kaizhangkai@didichuxing.com>
2019-04-02 01:21:09 +0800

    refactor code

--- dynamic.go
+++ dynamic.go
@@ -1,7 +1,32 @@
 package dynamic
 
+import (
+	"encoding/json"
+	"reflect"
+)
+
 // DynamicFielder is the dynamic fielder interface
 // Struct which implement this interface will have dynamic field support
 type DynamicFielder interface {
 	NewDynamicField(fieldName string) interface{}
+}
+
+var DynamicType = reflect.TypeOf(&Type{})
+
+type Type struct {
+	Value interface{}     `json:"-"`
+	raw   json.RawMessage `json:"-"`
+}
+
+func New(v interface{}) *Type {
+	return &Type{Value: v}
+}
+
+func (t *Type) UnmarshalJSON(data []byte) error {
+	t.raw = data
+	return nil
+}
+
+func (t *Type) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Value)
 }
